ac8f8d0a7b3e158155bbc8ec221339da73232023
Francis Russell <francis@unchartedbackwaters.co.uk>
2023-11-06 23:19:08 +0000

    Simplify UTF-16LE encoding.

--- src/model/unicode.rs
+++ src/model/unicode.rs
@@ -1,6 +1,3 @@
-use crate::model::data::{Message, U16};
-use std::io::Cursor;
-
 /// Use to `to_unicode` function for String
 pub trait Unicode {
     fn to_unicode(&self) -> Vec<u8>;
@@ -16,11 +13,6 @@ impl Unicode for String {
     /// assert_eq!(s.to_unicode(), [102, 0, 111, 0, 111, 0])
     /// ```
     fn to_unicode(&self) -> Vec<u8> {
-        let mut result = Cursor::new(Vec::new());
-        for c in self.encode_utf16() {
-            let encode_char = U16::LE(c);
-            encode_char.write(&mut result).unwrap();
-        }
-        result.into_inner()
+        self.encode_utf16().flat_map(u16::to_le_bytes).collect()
     }
 }
